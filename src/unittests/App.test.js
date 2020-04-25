0b1c300dbb8eda9aaf505f43b4ed540bf7bcdb38
temaari <53286793+temaari@users.noreply.github.com>
2020-04-25 16:36:54 +1200

    adding a console log to make the test more evident

--- src/unittests/App.test.js
+++ src/unittests/App.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('renders learn react link', () => {
+  console.log("This is a working test from the test suite")
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
