6dae2b2bd8c98f51542bcc6f6968020b16ea8029
temaari <53286793+temaari@users.noreply.github.com>
2020-04-25 17:47:55 +1200

    testing out again

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React for tips
+          Learn React
         </a>
       </header>
     </div>
