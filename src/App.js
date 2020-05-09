822f669826779b6cc0819f680f44ba13c869b910
temaari <53286793+temaari@users.noreply.github.com>
2020-05-09 13:37:06 +1200

    making the tap button and tap counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter:0
+    };
+    this.countTaps = this.countTaps.bind(this);
+  }
+
+  countTaps() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  render() {
+    return (  
+      <div className="App">
+      <div className="App-header">
+        <input className="App-Button" type="Button" value="Tap me" onClick={this.countTaps} />
+        <p>{this.state.counter}</p>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
